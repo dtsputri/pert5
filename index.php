8bfb081eeee9ad70649835e97ee1f158063a9949
Putri <dtsputrirosimayasari@outlook.com>
2020-10-05 20:17:26 +0700

    menambahkan file index.php

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name ="viewport" content="widht=device-width, inital-scale=1.0">
+    <title> LATIHAN GIT REMOTE </title>
+</head>
+<body>
+    <h1> Hellow World </h1>
+</body>
+</html>
